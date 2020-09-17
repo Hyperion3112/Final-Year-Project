8a2068b379903cecedba9c5adf39410416b9c533
tejas366 <63698581+tejas366@users.noreply.github.com>
2020-09-17 20:23:23 +0530

    Update signin.js

--- src/signin.js
+++ src/signin.js
@@ -100,7 +100,7 @@ class Signin extends React.Component {
   };
 
   onSubmit = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("http://localhost:3000/", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
