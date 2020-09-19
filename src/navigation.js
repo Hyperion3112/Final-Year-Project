266018780b02257518c9db88b5bc3ff172483e00
Hyperion3112 <48784043+Hyperion3112@users.noreply.github.com>
2020-09-19 12:54:17 +0530

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -1,13 +1,31 @@
-import react from "react";
+import React from "react";
 
-const Navigation = ({ onRouteChange, IsSignedIn }) => {
-  if (IsSignedIn) {
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
     return (
       <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-        <p>
-          {" "}
-          onClick={onRouteChange} className='f3 link dim black underline pa3
-          pointer'
+        <p
+          onClick={() => onRouteChange("signout")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange("register")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
         </p>
       </nav>
     );
