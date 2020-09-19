f3f57e1582513a8387fbd21edcba99368d574134
Hyperion3112 <48784043+Hyperion3112@users.noreply.github.com>
2020-09-19 23:33:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,81 @@
 import React, { Component } from "react";
 import Signin from "./signin";
-//import Navigation from "./navigation";
+import Signin2 from "./signin2";
+import Register from "./register";
+import Navigation from "./navigation";
+
 import "./App.css";
 import "tachyons";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      input: "",
+      route: "signin",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
+    };
   }
 
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
+    const { isSignedIn, route } = this.state;
     return (
       <div className="App">
-        <Signin />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Signin
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            )
+          </div>
+        ) : route === "signin" ? (
+          <Signin2
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
