2fb426426b983cb1fdac054d2481ba45ccd29630
Shaunak Divati <shaunakdivati@gmail.com>
2020-09-20 15:04:11 +0530

    init

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+  users: [
+    {
+      id: "1",
+      name: "max",
+      email: "max@gmail.com",
+      password: "red",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "2",
+      name: "luke",
+      email: "luke@gmail.com",
+      password: "brown",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.roll1 === database.users[0].roll1 &&
+    req.body.name1 === database.users[0].name1 &&
+    req.body.email1 === database.users[0].email1 &&
+    req.body.phone1 === database.users[0].phone1 &&
+    req.body.password1 === database.users[0].password1 &&
+    req.body.domain1 === database.users[0].domain1
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/signin2", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { roll, email } = req.body;
+  database.users.push({
+    id: "3",
+    roll: roll,
+    email: email,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+/*app.get("profile/ :id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+*/
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
---
+++ src/Register.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      roll: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  onRollChange = (event) => {
+    this.setState({
+      roll: event.target.value,
+    });
+  };
+  onEmailChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+  onPasswordChange = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  onSubmit = () => {
+    console.log(this.state);
+    fetch("http://localhost:3000/register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        roll: this.state.roll,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
+        }
+      });
+  };
+
+  render() {
+    //const { onRouteChange } = this.props;
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <form className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="Name">
+                  Roll Number
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-75"
+                  type="text"
+                  name="Roll"
+                  id="Roll"
+                  onChange={this.onRollChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-75"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-75"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmit}
+                className="br3 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+                //onClick={() => onRouteChange("home")}
+              />
+            </div>
+          </form>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Register;
